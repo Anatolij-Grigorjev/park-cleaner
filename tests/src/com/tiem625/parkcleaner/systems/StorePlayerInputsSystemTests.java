2948fe69573bf7951783c34c015dec32c2922a4b
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2023-09-23 15:48:39 +0300

    ideation on input

--- tests/src/com/tiem625/parkcleaner/systems/StorePlayerInputsSystemTests.java
+++ tests/src/com/tiem625/parkcleaner/systems/StorePlayerInputsSystemTests.java
@@ -1,20 +1,21 @@
 package com.tiem625.parkcleaner.systems;
 
 import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.tiem625.parkcleaner.components.PlayerInputComponent;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import com.tiem625.parkcleaner.domain.GameKey;
+import org.junit.jupiter.api.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class StorePlayerInputsSystemTests {
 
     Entity testEntity;
     StorePlayerInputsSystem system;
-
+    
     @BeforeEach
     void setupEntity() {
+
         testEntity = new Entity();
         testEntity.add(new PlayerInputComponent());
 
@@ -23,6 +24,10 @@ public class StorePlayerInputsSystemTests {
 
     @Test
     public void system_records_pressed_gamekeys() {
-
+        var testActionGameKey = new GameKey("TestAction", Input.Keys.T);
+        //TODO: set key pressed
+        Assertions.assertFalse(testEntity.getComponent(PlayerInputComponent.class).isKeyPressed(testActionGameKey));
+        system.processEntity(testEntity, Gdx.graphics.getDeltaTime());
+        Assertions.assertTrue(testEntity.getComponent(PlayerInputComponent.class).isKeyPressed(testActionGameKey));
     }
 }
